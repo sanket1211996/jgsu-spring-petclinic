ed34993c0d1ee868220de67df98ea6437cbcdd68
Sanket A Darwante <sanketdarwante6@gmail.com>
2022-03-26 21:14:21 -0400

    Added JenkinRunner Config Example

---
+++ pipeline.groovy
@@ -0,0 +1,35 @@
+pipeline {
+    agent any
+    
+    triggers { pollSCM('* * * * *') }
+
+    // tools {
+    //     // Install the Maven version configured as "M3" and add it to the path.
+    //     maven "M3"
+    // }
+
+    stages {
+        stage('Build') {
+            steps {
+                // Get some code from a GitHub repository
+                git branch: 'main', url: 'https://github.com/sanket1211996/jgsu-spring-petclinic.git'
+
+                // // Run Maven on a Unix agent.
+                // sh "mvn -Dmaven.test.failure.ignore=true clean package"
+                sh './mvnw clean package'
+
+                // To run Maven on a Windows agent, use
+                // bat "mvn -Dmaven.test.failure.ignore=true clean package"
+            }
+
+            post {
+                // If Maven was able to run the tests, even if some of the test
+                // failed, record the test results and archive the jar file.
+                success {
+                    junit '**/target/surefire-reports/TEST-*.xml'
+                    archiveArtifacts 'target/*.jar'
+                }
+            }
+        }
+    }
+}
